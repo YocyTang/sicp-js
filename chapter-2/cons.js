9e91989cc1fbbba9c194a32e013b38d557b5635e
Yocy <codehuman@163.com>
2016-10-18 21:33:07 +0800

    使用js构造scheme中的序对

---
+++ chapter-2/cons.js
@@ -0,0 +1,23 @@
+function cons(x, y){
+	return	function dispatch(m){
+		if(m === 0){
+			return x
+		} else if( m === 1){
+			return y
+		}else {
+			console.log(error)
+		}
+	}
+}
+function car(z){
+	return z(0)
+}
+function cdr(z){
+	return z(1)
+}
+
+module.exports={
+	cons,
+	car,
+	cdr
+}
