63067ac77145cf6d978aa5a66bca2639414ad887
Yocy <codehuman@163.com>
2016-10-24 19:43:23 +0800

    2-27

---
+++ chapter-2/2-27.js
@@ -0,0 +1,50 @@
+function cons(x, y){
+	return	function dispatch(m){
+		if(m === 0){
+			return x
+		} else if( m === 1){
+			return y
+		}else {
+			console.log(error)
+		}
+	}
+}
+function car(z){
+	return z(0)
+}
+function cdr(z){
+	return z(1)
+}
+function list(){
+	var arr = [].slice.call(arguments)
+	var n = arr.length
+	var ret
+	for(var i = n-1; i>=0; i--){
+		if( i === n-1){
+			ret = cons(arr[i], null)
+		}
+		else{
+			ret = cons(arr[i], ret)
+		}
+	}
+	return ret
+}
+function isPairs(a){
+ 	try{
+ 		car(a)
+ 		return true
+ 	}catch(e){
+ 		return false 
+ 	}
+ }
+
+
+ function deepReverse(a){
+ 	if(a === null){
+ 		return null
+ 	}else if(isPairs(a)){
+ 		return reverse(a)
+ 	}else{
+ 		return reverse(list(deepReverse(car(a)), deepReverse(car(cdr(a)))))
+ 	}
+ }
